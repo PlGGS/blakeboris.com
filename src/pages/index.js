e12a6f2c2b6ce1f7632613c1307806feb4956248
PlGGS <bbor0422@gmail.com>
2022-06-30 01:19:21 -0500

    Overhauled homepage scaling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,90 +1,102 @@
-import Layout from '../components/layout';
-import { motion } from "framer-motion"
+import Layout from "../components/layout";
+import { motion } from "framer-motion";
 
 const moveDuration = 2;
 
 const Index = () => (
     <Layout>
         <div id="body">
-            <table>
-                <tbody>
-                    <tr>
-                        <td id="intro">
-                            <motion.div
-                                initial={{
-                                    x: -1500,
-                                    y: 0
-                                }}
-                                animate={{
-                                    x: 0,
-                                    y: 150
-                                }}
-                                transition={{
-                                    duration: moveDuration,
-                                    delay: 0.2
-                                }}
-                            >
-                                <h1>Hello there, I'm <b>Blake Boris</b></h1>
-                            </motion.div>
-                            <motion.div 
-                                initial={{
-                                    x: -1500,
-                                    y: 0
-                                }}
-                                animate={{
-                                    x: 0,
-                                    y: 150
-                                }}
-                                transition={{
-                                    duration: moveDuration,
-                                    delay: 0.3
-                                }}
-                            >
-                                <h4>I'm a software engineer.</h4>
-                            </motion.div>
-                        </td>
-                        <td>
-                            <motion.div
-                                initial={{
-                                    x: 1500,
-                                    y: 0
-                                }}
-                                animate={{
-                                    x: 0,
-                                    y: 150
-                                }}
-                                transition={{
-                                    duration: moveDuration,
-                                    delay: 0.4
-                                }}
-                            >
-                                <img src="/blake.png" alt="blake" align="right"></img>
-                            </motion.div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div id="mid">
+                <div id="intro">
+                    <motion.div
+                        initial={{
+                            x: -1500,
+                            y: 0,
+                        }}
+                        animate={{
+                            x: 0,
+                            y: 150,
+                        }}
+                        transition={{
+                            duration: moveDuration,
+                            delay: 0.2,
+                        }}
+                    >
+                        <h1>
+                            Hello there, I'm <b>Blake Boris</b>
+                        </h1>
+                    </motion.div>
+                    <motion.div
+                        initial={{
+                            x: -1500,
+                            y: 0,
+                        }}
+                        animate={{
+                            x: 0,
+                            y: 150,
+                        }}
+                        transition={{
+                            duration: moveDuration,
+                            delay: 0.3,
+                        }}
+                    >
+                        <h4>I'm a software engineer.</h4>
+                    </motion.div>
+                </div>
+                <div id="pic">
+                    <motion.div
+                        initial={{
+                            x: 1500,
+                            y: 0,
+                        }}
+                        animate={{
+                            x: 0,
+                            y: 150,
+                        }}
+                        transition={{
+                            duration: moveDuration,
+                            delay: 0.4,
+                        }}
+                    >
+                        <img src="/blake.png" alt="blake" align="right"></img>
+                    </motion.div>
+                </div>
+            </div>
         </div>
         <style jsx>{`
             #body {
-                padding-left: 5%;
-                padding-right: 5%;
+                padding-left: 10%;
+                padding-right: 10%;
                 overflow: hidden;
             }
             #body table {
                 position: relative;
                 margin: 0 auto;
             }
-            #body table tbody tr td img {
+            #body img {
                 width: 400px;
                 border-radius: 50%;
             }
-            #body table tbody tr td {
+            #body {
                 padding: 20px;
                 padding-bottom: 150px;
             }
 
-            @media (max-device-width : 1080px) {
+            #mid {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+            #intro {
+                padding: 2%;
+                /* transform: translate(0px, 150px); */
+            }
+            #pic {
+                padding: 2%;
+            }
+
+            /* @media (max-device-width: 1080px) {
                 #body table tbody tr td img {
                     width: 100%;
                     border-radius: 50%;
@@ -93,14 +105,47 @@ const Index = () => (
                     padding-left: 10%;
                     padding-right: 10%;
                 }
-                #intro h1 {
+            } */
+
+            @media (max-device-width: 1080px) {
+                #body {
+                    display: flex;
+                    flex-flow: column;
                 }
-                #intro h4 {
+                #body img {
+                    border-radius: 50%;
+                }
+
+                #intro {
+                    position: absolute;
+                    bottom: 0;
+                }
+                #mid {
+                }
+
+                #pic {
+                    padding: 0px;
+                    order: 1;
+                    transform: translate(0px, calc(-10vh));
+                }
+                #pic img {
+                    width: 314px;
+                }
+                #intro {
+                    padding: 0px;
+                    order: 2;
+                    transform: translate(0px, calc(-40vh));
+                    text-align: center;
                 }
             }
 
-            @media (max-device-width: 1024px) {
-                transform: translate(0%, -2%);
+            @media screen and (max-height: 800px) {
+                #pic img {
+                    width: 220px;
+                }
+                #intro h1 {
+                    font-size: 30pt;
+                }
             }
         `}</style>
     </Layout>
