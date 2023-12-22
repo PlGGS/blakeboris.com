b0ca3d58995fd1847ef7db4f2e7d9e5305477455
PlGGS <bbor0422@gmail.com>
2023-12-22 13:52:42 -0600

    Updated Resume

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 import Layout from '../components/layout';
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function PdfViewer() {
     const [numPages, setNumPages] = useState(null);
@@ -13,20 +14,20 @@ function PdfViewer() {
     }
 
     return (
-        <div style={{
+        <div id='container' style={{
             display: 'flex',
             alignItems: 'center',
             justifyContent: 'center',
         }}>
-        <Document file="Resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-            <div style={{
-            boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.3)',
-            transform: 'scale(1.1)',
-            marginTop: '2.5em'
-        }}>
-            <Page pageNumber={pageNumber} renderTextLayer={false} />
-        </div>
-        </Document>
+            <Document file="Resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+                <div id='doc' style={{
+                    boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.3)',
+                    // transform: 'scale(1.1)',
+                    marginTop: '2.5em'
+                }}>
+                    <Page pageNumber={pageNumber} renderTextLayer={false} />
+                </div>
+            </Document>
         </div>
     );
 }
