5da1b200c7f8d8ca240bd3c2cc9dad2dc6780de8
PlGGS <bbor0422@gmail.com>
2025-01-15 18:51:46 -0600

    Dynamically display resume pages

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -5,10 +5,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-function PdfViewer({ scale }) {
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
-
+function PdfViewer({ scale, numPages, setNumPages }) {
     function onDocumentLoadSuccess({ numPages }) {
         setNumPages(numPages);
     }
@@ -16,17 +13,22 @@ function PdfViewer({ scale }) {
     return (
         <div id='container' style={{
             display: 'flex',
+            flexDirection: 'column',
             alignItems: 'center',
             justifyContent: 'center',
         }}>
-            <Document file="Resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-                <div id='doc' style={{
-                    boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.3)',
-                    // transform: 'scale(1.1)',
-                    marginTop: '2.5em'
-                }}>
-                    <Page pageNumber={pageNumber} renderTextLayer={false} scale={scale} />
-                </div>
+            <Document 
+                file="Resume.pdf" 
+                onLoadSuccess={onDocumentLoadSuccess} 
+            >
+                {[...Array(numPages).keys()].map((index) => (
+                    <div key={index} id='doc' style={{
+                        boxShadow: '0px 2px 6px rgba(0, 0, 0, 0.3)',
+                        marginTop: '2.5em'
+                    }}>
+                        <Page pageNumber={index + 1} renderTextLayer={false} scale={scale} />
+                    </div>
+                ))}
             </Document>
         </div>
     );
@@ -34,23 +36,16 @@ function PdfViewer({ scale }) {
 
 const Resume = () => {
     const [scale, setScale] = useState(1.3);
+    const [numPages, setNumPages] = useState(null);
 
     useEffect(() => {
         const handleResize = () => {
-            if (window.innerWidth <= 1080) {
-                setScale(0.6);
-            } else {
-                setScale(1.3);
-            }
+            setScale(window.innerWidth <= 1080 ? 0.6 : 1.3);
         };
 
-        // Initial scale setting
         handleResize();
-
-        // Event listener for window resize
         window.addEventListener('resize', handleResize);
 
-        // Cleanup the event listener on component unmount
         return () => {
             window.removeEventListener('resize', handleResize);
         };
@@ -58,15 +53,8 @@ const Resume = () => {
 
     return (
         <Layout>
-            <PdfViewer scale={scale} />
-            <style jsx>{`
-                @media (max-width: 1080px) {
-                    .pdf-container {
-                        width: 80%;
-                        margin: 0 auto;
-                    }
-                }
-            `}</style>
+            <PdfViewer style={{ paddingBottom: '1200rem' }} scale={scale} numPages={numPages || 1} setNumPages={setNumPages} />
+            <div style={{ height: '3rem' }} />
         </Layout>
     );
 };
